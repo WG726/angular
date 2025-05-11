0a288ce1f7664430758f1b5fb9fb08e6982e0b3f
Wayne Garrus III <wg3@Waynes-Macbook.local>
2025-05-11 16:20:09 -0700

    added the default home page

--- homes-app/src/app/routes.ts
+++ homes-app/src/app/routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 
-const routeConfig: Routes = [];
+const routeConfig: Routes = [
+    {
+        path: '', // empty because we want this to be the default page
+        component: HomeComponent,
+        title: 'Home Page'
+
+    }
+];
 
 export default routeConfig;
