cfc9db5f886b97eacc79f56bd5dfb00327eaf791
Wayne Garrus III <wg3@Waynes-Macbook.local>
2025-05-12 16:44:00 -0700

    event binding the form using the submit action calling the submitApplication function

--- homes-app/src/app/details/details.component.ts
+++ homes-app/src/app/details/details.component.ts
@@ -26,7 +26,17 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
       </section>
       <section class="listing-apply">
         <h2 class="section-heading">Apply now to live here</h2>
-        <button class="primary" type="button">Apply Now</button>
+        <form [formGroup]="applyForm" (submit)="submitApplication()"> <!-- event binding "(submit)" -->
+          <label for="first-name">First Name</label>
+          <input id="first-name" type="text" formControlName="firstName">
+
+          <label for="last-name">Last Name</label>
+          <input id="last-name" type="text" formControlName="lastName">
+
+          <label for="email">Email</label>
+          <input id="email" type="email" formControlName="email">
+          <button type="submit" class="primary">Apply Now</button>
+        </form>
       </section>
     </article>
 
@@ -42,9 +52,17 @@ export class DetailsComponent {
     lastName: new FormControl(''),
     email: new FormControl('')
   });
-  
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocatinById(housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
   }
 }
