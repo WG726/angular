1ba9c1c163f80708781dc4c9d3686241fa9d7431
Wayne Garrus III <wg3@Waynes-Macbook.local>
2025-05-12 16:32:53 -0700

    Added FormControl and FormGroup

--- homes-app/src/app/details/details.component.ts
+++ homes-app/src/app/details/details.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
     <article>
       <img class="listing-photo" [src]="housingLocation?.photo" alt="Photo of location."/>
@@ -36,7 +37,12 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute); // getting the current route information
   housingService: HousingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
-
+  applyForm = new FormGroup({ // collection of form controls that make up a form
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
+  
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocatinById(housingLocationId);
