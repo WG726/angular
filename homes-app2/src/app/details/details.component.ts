47ec0bba321eb5b62a92c1a4f88ec8c126e8feb7
Wayne Garrus III <waynegarrus3@outlook.com>
2025-06-21 00:08:51 -0700

    Adding HousingService to Details page

--- homes-app2/src/app/details/details.component.ts
+++ homes-app2/src/app/details/details.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-details',
@@ -8,16 +10,19 @@ import { ActivatedRoute } from '@angular/router';
   imports: [CommonModule],
   template: `
     <p>
-      details works! {{ housingLocationId }}
+      details works! {{ housingLocation?.id }}
     </p>
+    <!-- above we use the '?' because the variable can be undefinded -->
   `,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute); // getting access to the URL
-  housingLocationId = 0;
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
 
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.params['id']);
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
   }
 }
