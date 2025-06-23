3d21cc1077d292589ecb757e36229e9e57ec8e92
WG0726 <waynegarrus3@outlook.com>
2025-06-22 23:38:19 -0700

    Learned about spy and mocking services

--- practice-app/Notes.md
+++ practice-app/Notes.md
@@ -1,8 +1,12 @@
 # Unit Testing
-- Jasmine: testing framework that supports Behavior-Driven Development. Uses Karma to run tests.
-- Karma: default test runner for applications created using Angular CLI
+- <u>Jasmine</u>: testing <u>*framework*</u> that supports Behavior-Driven Development. Uses Karma to run tests.
+- <u>Karma</u>: default <u>*test runner*</u> for applications created using Angular CLI
     - controls creation of HTML files, opening web browsers, execution of tests and return of test results
 - **Spec** files are used for testing
+- SpyOn
+    - makes sure that the function is called on without actually calling the function
+    - "jasmine.createSpyObj(...)" creates a mock of a service
+    Note: mock a dependency, not the actual service/thing you are trying to test
 
 # Angular CLI Commands
 - Run tests: `ng test`
--- practice-app/src/app/calc.service.spec.ts
+++ practice-app/src/app/calc.service.spec.ts
@@ -1,9 +1,20 @@
 import { CalcService } from "./calc.service";
+import { SharedService } from "./shared.service";
 
 describe("CalcService", () => { // <- description of test group
-  it("should multiply two numbers", () => { // <- description of specific test ("spec")
-    const calc = new CalcService();
-    const result = calc.multiply(3, 5);
-    expect(result).toBe(15);
+  // it("should multiply two numbers", () => { // <- description of specific test ("spec")
+  //   const shared = new SharedService(); // dependency (CalcService is dependent on SharedService)
+  //   const calc = new CalcService(shared);
+  //   const result = calc.multiply(3, 5);
+  //   expect(result).toBe(15);
+  // });
+
+  it("should call the mySharedFunction func", () => { // making sure that the dependency function is called on
+    // const shared = new SharedService();
+    const shared = jasmine.createSpyObj("SharedService", ["mySharedFunction"]); // <- creating mock service
+    // spyOn(shared, "mySharedFunction"); // <- have to spy before the function is called. 
+    const calc = new CalcService(shared);
+    const result = calc.multiply(3, 5); // <- dependency function (mySharedFunction) is not actually being called.
+    expect(shared.mySharedFunction).toHaveBeenCalled();
   });
 });
