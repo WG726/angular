e8dde027a84c5ebba0f4a15d4da088969a5da1c4
WG0726 <waynegarrus3@outlook.com>
2025-06-23 20:19:09 -0700

    small comment

--- practice-app/src/app/calc.service.spec.ts
+++ practice-app/src/app/calc.service.spec.ts
@@ -14,7 +14,7 @@ describe("CalcService", () => { // <- description of test group
     const shared = jasmine.createSpyObj("SharedService", ["mySharedFunction"]); // <- creating mock service
     // spyOn(shared, "mySharedFunction"); // <- have to spy before the function is called. 
     const calc = new CalcService(shared);
-    const result = calc.multiply(3, 5); // <- dependency function (mySharedFunction) is not actually being called.
+    const result = calc.multiply(3, 5); // <- dependency function (mySharedFunction) is not actually being called. If you use spyOn or createSpyObj the function won't be called.
     expect(shared.mySharedFunction).toHaveBeenCalled();
   });
 });
